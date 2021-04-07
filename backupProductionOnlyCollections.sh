f1ee0cc016757b9a9d89d6316d217b66f4ad0639
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-04-07 17:43:44 -0500

    updating project dev environment

--- backupProductionOnlyCollections.sh
+++ backupProductionOnlyCollections.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 gcloud config set project ebombo-events
-gcloud beta firestore export gs://ebombo-events-backups/games/7apri --collection-ids='games'
+gcloud beta firestore export gs://ebombo-events.appspot.com/backup/7apri --collection-ids='games,settings,landings'
--- importProductionToDev.sh
+++ importProductionToDev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-gcloud config set project ebombo-events
-gcloud firestore import gs://ebombo-events-landings/25marzv1
+gcloud config set project ebombo-events-dev
+gcloud firestore import gs://ebombo-events.appspot.com/backup/7apri
