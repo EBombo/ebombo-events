7cf6acb6df1133509fac42f924cefae2ad9f9ebd
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-04-07 13:37:43 -0500

    adding canvas

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -24,6 +24,38 @@ export default (props) => {
         fetchLandingEvents();
     }, []);
 
+    useEffect(() => {
+        if (!loading) {
+            fillCanvas();
+        }
+    }, [loading])
+
+    const fillCanvas = () => {
+        let canvas = document.getElementById("landing-canvas");
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        let width = canvas.width;
+        let height = canvas.height;
+        let ctx = canvas.getContext("2d");
+
+        ctx.clearRect(0, 0, width, height);
+        for (let i = 0; i < 60; i++) {
+            let x = Math.floor(Math.random() * width);
+            let y = Math.floor(Math.random() * height);
+            let radius = Math.floor(Math.random() * 20);
+
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+
+            ctx.beginPath();
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+
     const fetchLandingEvents = () =>
         firestore
             .collection("landings")
@@ -74,6 +106,8 @@ export default (props) => {
                     <Footer marginLeft={"0"}/>
                 </ThemeProvider>
             </FooterSection>
+
+            <CanvasContainer id={"landing-canvas"}/>
         </LandingContainer>
     );
 };
@@ -85,7 +119,17 @@ const LandingContainer = styled.div`
   #10002b 9.9%,
   #100045 46.35%,
   #0e0063 100%);
+  position: relative;
 `;
+
+const CanvasContainer = styled.canvas`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+`
 
 const FooterSection = styled.section`
   width: 100%;
