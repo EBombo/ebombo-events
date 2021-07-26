15e92b44c99a4df0254aaeab137ba316f3dd0e04
PabloYncioR <pabloynciorivera@gmail.com>
2021-07-26 15:25:50 -0500

    fix warning

--- src/pages/home/EditSpecials.js
+++ src/pages/home/EditSpecials.js
@@ -1,150 +1,151 @@
-import React, { useState } from "reactn";
+import React, {useState} from "reactn";
 import styled from "styled-components";
-import { FileUpload, Input, TextArea } from "../../components";
-import { useForm } from "react-hook-form";
-import { string, object } from "yup";
+import {ButtonBombo, FileUpload, Input, TextArea} from "../../components";
+import {useForm} from "react-hook-form";
+import {object, string} from "yup";
 import get from "lodash/get";
-import { ButtonBombo } from "../../components";
-import { firestore } from "../../firebase";
+import {firestore} from "../../firebase";
 import defaultTo from "lodash/defaultTo";
 
-export default (props) => {
-  const [imageUrl, setImageUrl] = useState(null);
-  const [loading, setLoading] = useState(false);
+const EditSpecials = (props) => {
+    const [imageUrl, setImageUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  const schema = object().shape({
-    description: string().required(),
-    name: string().required(),
-  });
-
-  const { register, handleSubmit, errors } = useForm({
-    validationSchema: schema,
-    reValidateMode: "onSubmit",
-  });
-
-  const saveComment = async (data) => {
-    setLoading(true);
-
-    let elements;
-
-    if (
-      defaultTo(get(props, `events.${props.currentField}`), []).some(
-        (special) => special.id === props.currentElement.id
-      )
-    ) {
-      elements = defaultTo(
-        get(props, `events.${props.currentField}`),
-        []
-      ).map((special) =>
-        special.id === props.currentElement.id
-          ? mapElement(data, special)
-          : special
-      );
-    } else {
-      elements = defaultTo(get(props, `events.${props.currentField}`), []);
-      elements.push(mapElement(data));
-    }
-
-    console.log(props.currentField, elements);
-
-    await firestore.doc(`landings/events`).update({
-      [props.currentField]: elements,
+    const schema = object().shape({
+        description: string().required(),
+        name: string().required(),
     });
 
-    props.setIsVisibleModal(false);
-    setLoading(false);
-  };
+    const {register, handleSubmit, errors} = useForm({
+        validationSchema: schema,
+        reValidateMode: "onSubmit",
+    });
 
-  const mapElement = (data, oldElement = null) => {
-    if (oldElement) {
-      const element = {
-        ...props.currentElement,
-        description: data.description,
-        name: data.name,
-      };
-      if (imageUrl) element["imageUrl"] = imageUrl;
-      return element;
-    }
-    return {
-      ...props.currentElement,
-      description: data.description,
-      name: data.name,
-      imageUrl: imageUrl,
+    const saveComment = async (data) => {
+        setLoading(true);
+
+        let elements;
+
+        if (
+            defaultTo(get(props, `events.${props.currentField}`), []).some(
+                (special) => special.id === props.currentElement.id
+            )
+        ) {
+            elements = defaultTo(
+                get(props, `events.${props.currentField}`),
+                []
+            ).map((special) =>
+                special.id === props.currentElement.id
+                    ? mapElement(data, special)
+                    : special
+            );
+        } else {
+            elements = defaultTo(get(props, `events.${props.currentField}`), []);
+            elements.push(mapElement(data));
+        }
+
+        console.log(props.currentField, elements);
+
+        await firestore.doc(`landings/events`).update({
+            [props.currentField]: elements,
+        });
+
+        props.setIsVisibleModal(false);
+        setLoading(false);
     };
-  };
 
-  return (
-    <Container>
-      <div className="title">
-        {props.currentField === "specialGuests" ? "Invitados" : "Regalos"}{" "}
-        Especiales
-      </div>
-      <form onSubmit={handleSubmit(saveComment)}>
-        <Input
-          variant="primary"
-          name="name"
-          ref={register}
-          error={errors.name}
-          required
-          label="Nombre:"
-          defaultValue={get(props, "currentElement.name", "")}
-          placeholder="Nombre"
-        />
-        <TextArea
-          variant="primary"
-          name="description"
-          ref={register}
-          error={errors.description}
-          required
-          label="Descripción:"
-          defaultValue={get(props, "currentElement.description", "")}
-          placeholder="Descripción del juego"
-        />
-        <div className="image-component">
-          <FileUpload
-            file={get(props, "currentElement.imageUrl", "")}
-            fileName="imageUrl"
-            filePath={`/events/specials/${props.currentElement.id}`}
-            bucket="landings"
-            sizes={get(props, "sizes", "500x500")}
-            afterUpload={(imageUrls) => setImageUrl(imageUrls[0].url)}
-          />
-        </div>
-        <div className="buttons-container">
-          <ButtonBombo
-            variant="contained"
-            color="primary"
-            margin="0"
-            loading={loading}
-            disabled={loading}
-            htmlType="submit"
-          >
-            Guardar
-          </ButtonBombo>
-          <ButtonBombo
-            variant="outlined"
-            color="danger"
-            margin="0"
-            loading={loading}
-            disabled={loading}
-            onClick={() => props.setIsVisibleModal(false)}
-          >
-            Cancelar
-          </ButtonBombo>
-        </div>
-      </form>
-    </Container>
-  );
+    const mapElement = (data, oldElement = null) => {
+        if (oldElement) {
+            const element = {
+                ...props.currentElement,
+                description: data.description,
+                name: data.name,
+            };
+            if (imageUrl) element["imageUrl"] = imageUrl;
+            return element;
+        }
+        return {
+            ...props.currentElement,
+            description: data.description,
+            name: data.name,
+            imageUrl: imageUrl,
+        };
+    };
+
+    return (
+        <Container>
+            <div className="title">
+                {props.currentField === "specialGuests" ? "Invitados" : "Regalos"}{" "}
+                Especiales
+            </div>
+            <form onSubmit={handleSubmit(saveComment)}>
+                <Input
+                    variant="primary"
+                    name="name"
+                    ref={register}
+                    error={errors.name}
+                    required
+                    label="Nombre:"
+                    defaultValue={get(props, "currentElement.name", "")}
+                    placeholder="Nombre"
+                />
+                <TextArea
+                    variant="primary"
+                    name="description"
+                    ref={register}
+                    error={errors.description}
+                    required
+                    label="Descripción:"
+                    defaultValue={get(props, "currentElement.description", "")}
+                    placeholder="Descripción del juego"
+                />
+                <div className="image-component">
+                    <FileUpload
+                        file={get(props, "currentElement.imageUrl", "")}
+                        fileName="imageUrl"
+                        filePath={`/events/specials/${props.currentElement.id}`}
+                        bucket="landings"
+                        sizes={get(props, "sizes", "500x500")}
+                        afterUpload={(imageUrls) => setImageUrl(imageUrls[0].url)}
+                    />
+                </div>
+                <div className="buttons-container">
+                    <ButtonBombo
+                        variant="contained"
+                        color="primary"
+                        margin="0"
+                        loading={loading}
+                        disabled={loading}
+                        htmlType="submit"
+                    >
+                        Guardar
+                    </ButtonBombo>
+                    <ButtonBombo
+                        variant="outlined"
+                        color="danger"
+                        margin="0"
+                        loading={loading}
+                        disabled={loading}
+                        onClick={() => props.setIsVisibleModal(false)}
+                    >
+                        Cancelar
+                    </ButtonBombo>
+                </div>
+            </form>
+        </Container>
+    );
 };
 
 const Container = styled.div`
   width: 100%;
   margin: 1rem 0;
+
   .title {
     font-size: 18px;
     line-height: 22px;
     color: ${(props) => props.theme.basic.white};
   }
+
   form {
     margin-top: 1rem;
 
@@ -161,3 +162,5 @@ const Container = styled.div`
     }
   }
 `;
+
+export default EditSpecials;
--- src/pages/home/SpecialGuests.js
+++ src/pages/home/SpecialGuests.js
@@ -1,116 +1,117 @@
-import React, { useGlobal, useState } from "reactn";
+import React, {useGlobal, useState} from "reactn";
 import styled from "styled-components";
-import { Divider } from "../../components/common/Divider";
+import {Divider} from "../../components/common/Divider";
 import defaultTo from "lodash/defaultTo";
 import get from "lodash/get";
-import { ButtonAnt } from "../../components/form";
-import { Carousel } from "../../components/common/Carousel";
-import { ModalContainer } from "../../components/common/ModalContainer";
-import { Icon } from "../../components/common/Icons";
-import { mediaQuery } from "../../styles/constants";
-import { Desktop, Tablet } from "../../constants";
-import { firestore } from "../../firebase";
+import {ButtonAnt} from "../../components/form";
+import {Carousel} from "../../components/common/Carousel";
+import {ModalContainer} from "../../components/common/ModalContainer";
+import {Icon} from "../../components/common/Icons";
+import {mediaQuery} from "../../styles/constants";
+import {Desktop, Tablet} from "../../constants";
+import {firestore} from "../../firebase";
 import chunk from "lodash/chunk";
 import EditSpecials from "./EditSpecials";
 
 export const SpecialGuests = (props) => {
-  const [authUser] = useGlobal("user");
-  const [currentElement, setCurrentElement] = useState(null);
-  const [currentField, setCurrentField] = useState(null);
-  const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [authUser] = useGlobal("user");
+    const [currentElement, setCurrentElement] = useState(null);
+    const [currentField, setCurrentField] = useState(null);
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
 
-  const guestContent = (guest) => (
-    <GuestContainer backgroundImage={guest.imageUrl}>
-      <div className="thumb">
-        <div className="mask" />
-      </div>
-      <div className="details">
-        <div className="name">{guest.name}</div>
-        <div className="description">{guest.description}</div>
-      </div>
-      {get(authUser, "isAdmin") && (
-        <div className="container-edit">
-          <Icon
-            className="icon-edit"
-            type="edit"
-            onClick={() => {
-              setCurrentElement(guest);
-              setCurrentField("specialGuests");
-              setIsVisibleModal(true);
-            }}
-          />
-          <Icon
-            className="icon-delete"
-            type="delete"
-            onClick={() => {
-              props.deleteElement(guest, "specialGuests");
-            }}
-          />
-        </div>
-      )}
-    </GuestContainer>
-  );
+    const guestContent = (guest) => (
+        <GuestContainer backgroundImage={guest.imageUrl}
+                        key={guest.name}>
+            <div className="thumb">
+                <div className="mask"/>
+            </div>
+            <div className="details">
+                <div className="name">{guest.name}</div>
+                <div className="description">{guest.description}</div>
+            </div>
+            {get(authUser, "isAdmin") && (
+                <div className="container-edit">
+                    <Icon
+                        className="icon-edit"
+                        type="edit"
+                        onClick={() => {
+                            setCurrentElement(guest);
+                            setCurrentField("specialGuests");
+                            setIsVisibleModal(true);
+                        }}
+                    />
+                    <Icon
+                        className="icon-delete"
+                        type="delete"
+                        onClick={() => {
+                            props.deleteElement(guest, "specialGuests");
+                        }}
+                    />
+                </div>
+            )}
+        </GuestContainer>
+    );
 
-  const carouselContent = (arrGuests) => (
-    <div>{arrGuests.map((guest) => guestContent(guest))}</div>
-  );
+    const carouselContent = (arrGuests) => (
+        <div>{arrGuests.map((guest) => guestContent(guest))}</div>
+    );
 
-  return (
-    <GuestsContainer>
-      {isVisibleModal && get(authUser, "isAdmin") && (
-        <ModalContainer
-          footer={null}
-          visible={isVisibleModal}
-          onCancel={() => setIsVisibleModal(!isVisibleModal)}
-        >
-          <EditSpecials
-            setIsVisibleModal={setIsVisibleModal}
-            isVisibleModal={isVisibleModal}
-            currentField={currentField}
-            currentElement={currentElement}
-            sizes={"250x300"}
-            {...props}
-          />
-        </ModalContainer>
-      )}
-      <Divider>
-        <div className="title">Time to Talk</div>
-      </Divider>
-      <div className="subtitle">Dínos a quién necesitas y lo trameos :) </div>
-      <Desktop>
-        <div className="wrapper">
-          {defaultTo(get(props, "events.specialGuests"), []).map((guest) =>
-            guestContent(guest)
-          )}
-        </div>
-      </Desktop>
-      <Tablet>
-        <Carousel
-          components={chunk(
-            defaultTo(get(props, "home.specialGuests"), []),
-            2
-          ).map((arrGuests) => carouselContent(arrGuests))}
-        />
-      </Tablet>
-      {get(authUser, "isAdmin") && (
-        <div className="btn-container">
-          <ButtonAnt
-            variant="outlined"
-            color="action"
-            onClick={() => {
-              setCurrentField("specialGuests");
-              setCurrentElement({
-                id: firestore.collection("events").doc().id,
-              });
-              setIsVisibleModal(true);
-            }}
-          >
-            Añadir
-          </ButtonAnt>
-        </div>
-      )}
-    </GuestsContainer>
-  );
+    return (
+        <GuestsContainer>
+            {isVisibleModal && get(authUser, "isAdmin") && (
+                <ModalContainer
+                    footer={null}
+                    visible={isVisibleModal}
+                    onCancel={() => setIsVisibleModal(!isVisibleModal)}
+                >
+                    <EditSpecials
+                        setIsVisibleModal={setIsVisibleModal}
+                        isVisibleModal={isVisibleModal}
+                        currentField={currentField}
+                        currentElement={currentElement}
+                        sizes={"250x300"}
+                        {...props}
+                    />
+                </ModalContainer>
+            )}
+            <Divider>
+                <div className="title">Time to Talk</div>
+            </Divider>
+            <div className="subtitle">Dínos a quién necesitas y lo trameos :)</div>
+            <Desktop>
+                <div className="wrapper">
+                    {defaultTo(get(props, "events.specialGuests"), []).map((guest) =>
+                        guestContent(guest)
+                    )}
+                </div>
+            </Desktop>
+            <Tablet>
+                <Carousel
+                    components={chunk(
+                        defaultTo(get(props, "home.specialGuests"), []),
+                        2
+                    ).map((arrGuests) => carouselContent(arrGuests))}
+                />
+            </Tablet>
+            {get(authUser, "isAdmin") && (
+                <div className="btn-container">
+                    <ButtonAnt
+                        variant="outlined"
+                        color="action"
+                        onClick={() => {
+                            setCurrentField("specialGuests");
+                            setCurrentElement({
+                                id: firestore.collection("events").doc().id,
+                            });
+                            setIsVisibleModal(true);
+                        }}
+                    >
+                        Añadir
+                    </ButtonAnt>
+                </div>
+            )}
+        </GuestsContainer>
+    );
 };
 
 const GuestsContainer = styled.div`
@@ -172,11 +173,13 @@ const GuestContainer = styled.div`
 
   .thumb {
     overflow: hidden;
+
     img {
       display: block;
       width: 100%;
       height: 100%;
     }
+
     .mask {
       background: rgba(75, 191, 135, 0.67);
       position: absolute;
@@ -188,6 +191,7 @@ const GuestContainer = styled.div`
       transition: all 0.3s ease-in-out;
     }
   }
+
   .details {
     color: ${(props) => props.theme.basic.white};
     position: absolute;
@@ -222,6 +226,7 @@ const GuestContainer = styled.div`
     .mask {
       opacity: 1;
     }
+
     .description {
       opacity: 1;
     }
