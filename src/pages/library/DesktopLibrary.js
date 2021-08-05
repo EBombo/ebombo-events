930c813c4405b41cb28fad2ef7196ca4e53ffe79
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-08-05 11:48:55 -0500

    folders

--- src/pages/library/DesktopLibrary.js
+++ src/pages/library/DesktopLibrary.js
@@ -2,15 +2,11 @@ import React, { useState } from "reactn";
 import styled from "styled-components";
 import { Image } from "../../components/common/Image";
 import { config } from "../../firebase";
-import { Input } from "../../components/form";
 import { useRouter } from "next/router";
-import isEmpty from "lodash/isEmpty";
 import get from "lodash/get";
-import { ListGameView } from "./ListGameView";
+import { DesktopLibraryGames } from "./DesktopLibraryGames";
 
 export const DesktopLibrary = (props) => {
-  const [listType, setListType] = useState("icons");
-  const [tab, setTab] = useState("all");
   const router = useRouter();
 
   return (
@@ -18,7 +14,9 @@ export const DesktopLibrary = (props) => {
       <div className="left-container">
         <div className="subtitle">Libreria</div>
         <div
-          className={`item games ${get(router, "query.item", "") === "games" ? "active" : ""}`}
+          className={`item games ${
+            get(router, "query.item", "") === "games" ? "active" : ""
+          }`}
           onClick={() =>
             router.push({ path: "library", query: { item: "games" } })
           }
@@ -33,7 +31,9 @@ export const DesktopLibrary = (props) => {
           <div className="name">Mis juegos</div>
         </div>
         <div
-          className={`item favorites ${get(router, "query.item", "") === "folders" ? "active" : ""}`}
+          className={`item favorites ${
+            get(router, "query.item", "") === "folders" ? "active" : ""
+          }`}
           onClick={() =>
             router.push({ path: "library", query: { item: "folders" } })
           }
@@ -48,63 +48,14 @@ export const DesktopLibrary = (props) => {
           <div className="name">Folders</div>
         </div>
       </div>
-      <div className="right-container">
-        <div className="nav-container">
-          <div className="tabs-container">
-            <div
-              className={`tab ${tab === "all" ? "active" : ""}`}
-              onClick={() => setTab("all")}
-            >
-              Mis juegos
-            </div>
-            <div
-              className={`tab middle ${tab === "favorites" ? "active" : ""}`}
-              onClick={() => setTab("favorites")}
-            >
-              Favoritos
-            </div>
-            <div
-              className={`tab ${tab === "drafts" ? "active" : ""}`}
-              onClick={() => setTab("drafts")}
-            >
-              Borradores
-            </div>
-          </div>
-
-          <div className="search-bar">
-            <Input
-              variant="clear"
-              placeholder="Buscar"
-              marginBottom="0"
-              border={`2px solid #C4C4C4`}
-              borderRadius="4px"
-              width="225px"
-            />
-          </div>
-
-          <div className="list-type">
-            <div className="icons" onClick={() => setListType("icons")}>
-              <div className={`${listType === "icons" ? "active" : ""}`} />
-              <div className={`${listType === "icons" ? "active" : ""}`} />
-            </div>
-            <div className="list" onClick={() => setListType("list")}>
-              <div className={`${listType === "list" ? "active" : ""}`} />
-              <div className={`${listType === "list" ? "active" : ""}`} />
-              <div className={`${listType === "list" ? "active" : ""}`} />
-            </div>
-          </div>
-        </div>
-
-        <div className="list-container">
-          {get(router, "query.item", "games") &&
-            props.games.map((game) => (
-              <ListGameView game={game} listType={listType} {...props} />
-            ))}
-          {isEmpty(props.games) && (
-            <div className="empty-container">No cuentas con juegos.</div>
-          )}
-        </div>
-      </div>
+      {get(router, "query.item", "games") === "games" && (
+        <DesktopLibraryGames {...props} />
+      )}
+      {
+        get(router, "query.item", "games") === "folders" && (
+            <DesktopLibraryFolders {...props} />
+        )
+      }
     </DesktopLibraryContainer>
   );
 };
@@ -114,98 +65,6 @@ const DesktopLibraryContainer = styled.div`
   height: calc(100vh - 50px);
   display: grid;
   grid-template-columns: 250px auto;
-
-  .right-container {
-    padding: 2rem;
-    background: ${(props) => props.theme.basic.whiteDark};
-
-    .list-container {
-      margin-top: 2rem;
-    }
-
-    .empty-container {
-      display: flex;
-      justify-content: center;
-      padding: 2rem;
-      font-family: Lato;
-      font-style: normal;
-      font-weight: normal;
-      font-size: 15px;
-      line-height: 18px;
-    }
-
-    .list-type {
-      display: flex;
-      align-items: center;
-
-      .icons {
-        margin-right: 5px;
-        cursor: pointer;
-        div {
-          border: 2px solid ${(props) => props.theme.basic.grayLighten};
-          box-sizing: border-box;
-          border-radius: 3px;
-          width: 35px;
-          height: 15px;
-          margin-bottom: 2px;
-        }
-
-        .active {
-          border: 2px solid ${(props) => props.theme.basic.secondary};
-        }
-      }
-
-      .list {
-        cursor: pointer;
-        div {
-          border: 2px solid ${(props) => props.theme.basic.grayLighten};
-          box-sizing: border-box;
-          border-radius: 3px;
-          width: 35px;
-          height: 9px;
-          margin-bottom: 2px;
-        }
-
-        .active {
-          border: 2px solid ${(props) => props.theme.basic.secondary};
-        }
-      }
-    }
-
-    .nav-container {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-
-      .tabs-container {
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-        border-radius: 4px;
-        box-sizing: border-box;
-        border: 2px solid ${(props) => props.theme.basic.grayLighten};
-
-        .tab {
-          padding: 0.5rem 1rem;
-          font-family: Lato;
-          font-style: normal;
-          font-weight: bold;
-          font-size: 15px;
-          line-height: 18px;
-          cursor: pointer;
-        }
-
-        .middle {
-          border-left: 2px solid ${(props) => props.theme.basic.grayLighten};
-          border-right: 2px solid ${(props) => props.theme.basic.grayLighten};
-        }
-        
-        .active{
-          background: ${props => props.theme.basic.whiteLight};
-        }
-      }
-    }
-  }
 
   .left-container {
     background: ${(props) => props.theme.basic.whiteLight};
@@ -234,8 +93,8 @@ const DesktopLibraryContainer = styled.div`
     .item:hover {
       background: ${(props) => props.theme.basic.whiteDark};
     }
-    
-    .active{
+
+    .active {
       background: ${(props) => props.theme.basic.whiteDark};
     }
 
---
+++ src/pages/library/DesktopLibraryFolders.js
@@ -0,0 +1,12 @@
+import React, { useState, useGlobal } from "reactn";
+import styled from "styled-components";
+
+export const DesktopLibraryFolders = (props) => {
+  return <FoldersContainer>
+      folders
+  </FoldersContainer>;
+};
+
+const FoldersContainer = styled.div`
+  width: 100%;
+`;
