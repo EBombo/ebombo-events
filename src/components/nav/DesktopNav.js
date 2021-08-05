c27c3902de42cba8ffa6891be38d70c59b371c7c
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-08-05 11:30:01 -0500

    fixing desktop nav

--- src/components/nav/DesktopNav.js
+++ src/components/nav/DesktopNav.js
@@ -1,4 +1,4 @@
-import React, { useGlobal } from "reactn";
+import React, { useGlobal, useEffect } from "reactn";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import { useAcl } from "../../hooks";
@@ -7,42 +7,49 @@ import { config } from "../../firebase";
 import { Image } from "../common/Image";
 import { Anchor } from "../form";
 import { sizes } from "../../constants";
+import { menus } from "../common/DataList";
 
 export const DesktopNav = (props) => {
   const router = useRouter();
-  const { userAcls } = useAcl();
+  const { aclMenus } = useAcl();
   const [authUser] = useGlobal("user");
   const [, setOpenRightDrawer] = useGlobal("openRightDrawer");
+
+  useEffect(() => {
+    !authUser && router.push("/");
+  }, []);
+
+  const getCurrentMenu = () =>
+    aclMenus({ menus: menus.filter((menu) => !menu.isAdmin) });
+
+  const isSelected = (path) =>
+    path === window.location.pathname ? "item item-selected" : "item";
 
   return (
     <DesktopNavContainer>
       <div className="items-container">
         <Image
           src={`${config.storageUrl}/resources/ebombo-white.svg`}
-          onClick={() =>
-            userAcls.some((acl) => acl.includes("admin"))
-              ? router.push("/admin")
-              : authUser
-              ? router.push("/library")
-              : router.push("/")
-          }
           height="23px"
           width="88px"
         />
-        {authUser && (
-          <div className="nav-items">
-            <ul>
-              <li
-                onClick={() =>
-                  router.push({ path: "library", query: { item: "games" } })
-                }
-              >
-                Librería
-              </li>
-              <li onClick={() => router.push("/reports")}>Reportes</li>
-            </ul>
-          </div>
-        )}
+        <div className="nav-items">
+          {getCurrentMenu().map((userLink) => (
+            <div
+              className={isSelected(userLink.url)}
+              key={`key-menu-user-link-${userLink.url}`}
+              onClick={() => router.push(userLink.url)}
+            >
+              <Image
+                src={userLink.src}
+                width="20px"
+                height="20px"
+                className="icon"
+              />
+              <span className="label">{userLink.name}</span>
+            </div>
+          ))}
+        </div>
       </div>
       {!authUser && (
         <Anchor onClick={() => setIsVisibleLoginModal(true)} variant="primary">
@@ -81,22 +88,38 @@ const DesktopNavContainer = styled.div`
       margin-left: 10px;
       height: 100%;
 
-      ul {
-        list-style: none;
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+
+      .item {
+        position: relative;
         display: flex;
         align-items: center;
-        justify-content: space-around;
+        justify-content: center;
+        color: ${(props) => props.theme.basic.white};
+        cursor: pointer;
+        padding: 0 0.5rem;
         height: 100%;
-        margin: 0;
 
-        li {
-          height: 100%;
-          padding: 0.5rem;
-          font-size: ${sizes.font.normal};
-          color: ${(props) => props.theme.basic.white};
-          display: flex;
-          align-items: center;
-          cursor: pointer;
+        .label {
+          font-size: ${sizes.font.small};
+          text-align: center;
+          margin-left: 3px;
+        }
+      }
+
+      .item-selected {
+        position: relative;
+        color: ${(props) => props.theme.basic.primaryLight};
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        border-bottom: 3px solid ${props => props.theme.basic.primaryLight};
+
+        .icon {
+          filter: opacity(0.5);
         }
       }
     }
