c2b9ce7c24e5ef3be98933d1d3542e67791d09b8
Pablo <tech@ebombo.com>
2021-10-26 03:48:28 +0000

    register - imagen desktop

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,7 +3,7 @@ import { Divider } from "../../components/common/Divider";
 import { Controller, useForm } from "react-hook-form";
 import React, { useEffect, useGlobal } from "reactn";
 import { useAuth } from "../../hooks/useAuth";
-import { mediaQuery, sizes } from "../../constants";
+import { mediaQuery, sizes, Desktop } from "../../constants";
 import styled from "styled-components";
 import { dialCodes } from "../../utils";
 import { Image } from "../../components/common/Image";
@@ -17,253 +17,262 @@ import { darkTheme } from "../../theme";
 import moment from "moment";
 
 export const Register = (props) => {
-  const validationSchema = object().shape({
-    name: string().required(),
-    lastName: string().required(),
-    email: string()
-      .required()
-      .email()
-      .test("", "Email invalid!", (email_) => !email_.includes("yopmail.com")),
-    password: string().required().min(6),
-    countryCode: string().required(),
-    phoneNumber: string().required().min(5),
-    day: string().required(),
-    month: string().required(),
-    year: string().required(),
-  });
-
-  const { signUp, ButtonsProviders } = useAuth();
-  const [authUser] = useGlobal("user");
-  const [isLoadingUser] = useGlobal("isLoadingUser");
-  const [isLoadingCreateUser] = useGlobal("isLoadingCreateUser");
-
-  const { register, errors, handleSubmit, control, watch } = useForm({
-    validationSchema,
-    reValidateMode: "onSubmit",
-  });
-
-  const router = useRouter();
-
-  useEffect(() => {
-    if (authUser) return router.push("/library");
-  }, [authUser]);
-
-  const dialCode = (countryCode) => {
-    const country = dialCodes.find((country) => country.code === countryCode);
-
-    return get(country, "dialCode", null);
-  };
-
-  const signUpUser = async (user) => {
-    await signUp({
-      ...user,
-      birthDate: moment(
-        `${user.day}/${user.month}/${user.year}`,
-        "DD/MM/YYYY"
-      ).toDate(),
+    const validationSchema = object().shape({
+        name: string().required(),
+        lastName: string().required(),
+        email: string()
+            .required()
+            .email()
+            .test("", "Email invalid!", (email_) => !email_.includes("yopmail.com")),
+        password: string().required().min(6),
+        countryCode: string().required(),
+        phoneNumber: string().required().min(5),
+        day: string().required(),
+        month: string().required(),
+        year: string().required(),
     });
-  };
 
-  return (
-    <RegisterContainer>
-      <form onSubmit={handleSubmit(signUpUser)} autoComplete="off" noValidate>
-        <Divider>
-          <div className="divider-content">
-            <Image
-              margin="0"
-              width="15px"
-              height="15px"
-              src={`${config.storageUrl}/resources/personal-info.svg`}
-              size="contain"
-            />
-            <div className="subtitle">Información Personal</div>
-          </div>
-        </Divider>
-        <div className="inputs-container">
-          <Input
-            error={errors.name}
-            type="text"
-            ref={register}
-            name="name"
-            autoComplete="off"
-            placeholder="Nombre"
-          />
-        </div>
-        <div className="inputs-container">
-          <Input
-            error={errors.lastName}
-            type="text"
-            ref={register}
-            name="lastName"
-            autoComplete="off"
-            placeholder="Apellidos"
-          />
-        </div>
-        <div className="inputs-container">
-          <div className="label">Fecha de Nacimiento</div>
-          <InputGroupSelect>
-            <Controller
-              name="day"
-              control={control}
-              as={
-                <Select
-                  placeholder="día"
-                  showSearch
-                  virtual={false}
-                  error={errors.day}
-                  borderRight={`0.1px solid ${darkTheme.basic.grayLighten}`}
-                  optionFilterProp="children"
-                  borderRadius="3px 0px 0px 3px"
-                  optionsdom={days.map((day) => ({
-                    key: day.key,
-                    name: day.value,
-                    code: day.value,
-                  }))}
-                />
-              }
-            />
-            <Controller
-              name="month"
-              control={control}
-              as={
-                <Select
-                  placeholder="mes"
-                  showSearch
-                  virtual={false}
-                  error={errors.month}
-                  borderRight={`0.1px solid ${darkTheme.basic.grayLighten}`}
-                  borderRadius="0px 0px 0px 0px"
-                  optionFilterProp="children"
-                  optionsdom={months.map((month) => ({
-                    key: month.key,
-                    code: month.key,
-                    name: month.value,
-                  }))}
-                />
-              }
-            />
-            <Controller
-              name="year"
-              control={control}
-              as={
-                <Select
-                  placeholder="año"
-                  showSearch
-                  virtual={false}
-                  borderRadius="0px 3px 3px 0px"
-                  error={errors.year}
-                  optionFilterProp="children"
-                  optionsdom={years.map((year) => ({
-                    key: year.key,
-                    name: year.value,
-                    code: year.value,
-                  }))}
-                />
-              }
-            />
-          </InputGroupSelect>
-        </div>
+    const { signUp, ButtonsProviders } = useAuth();
+    const [authUser] = useGlobal("user");
+    const [isLoadingUser] = useGlobal("isLoadingUser");
+    const [isLoadingCreateUser] = useGlobal("isLoadingCreateUser");
 
-        <Divider>
-          <div className="divider-content">
-            <Image
-              margin="0"
-              width="15px"
-              height="15px"
-              src={`${config.storageUrl}/resources/contact-info.svg`}
-              size="contain"
-            />
-            <div className="subtitle">Contacto</div>
-          </div>
-        </Divider>
-        <div className="inputs-container">
-          <Input
-            error={errors.email}
-            type="email"
-            ref={register}
-            name="email"
-            autoComplete="off"
-            placeholder="Correo"
-          />
-        </div>
-        <div className="inputs-container">
-          <InputGroup gridTemplateColumns="2fr 50px 3fr">
-            <Controller
-              name="countryCode"
-              control={control}
-              as={
-                <Select
-                  placeholder="Pais"
-                  showSearch
-                  virtual={false}
-                  error={errors.countryCode}
-                  optionFilterProp="children"
-                  optionsdom={getData().map((country) => ({
-                    key: country.code,
-                    code: country.code,
-                    name: country.name,
-                  }))}
-                />
-              }
-            />
-            <CountryCode>{dialCode(watch("countryCode"))}</CountryCode>
-            <Input
-              error={errors.phoneNumber}
-              type="number"
-              ref={register}
-              name="phoneNumber"
-              autoComplete="off"
-              placeholder="Celular"
-            />
-          </InputGroup>
-        </div>
-        <Divider>
-          <div className="divider-content">
-            <Image
-              margin="0"
-              width="15px"
-              height="15px"
-              src={`${config.storageUrl}/resources/access-data.svg`}
-              size="contain"
-            />
-            <div className="subtitle">Datos de acceso</div>
-          </div>
-        </Divider>
-        <div className="inputs-container">
-          <Input
-            error={errors.password}
-            type="password"
-            ref={register}
-            name="password"
-            autoComplete="off"
-            placeholder="Contraseña"
-          />
-          <ButtonAnt
-            loading={isLoadingCreateUser}
-            disabled={isLoadingUser || isLoadingCreateUser}
-            variant="contained"
-            htmlType="submit"
-            width="100%"
-            margin="0.5rem 0"
-          >
-            Registrar
+    const { register, errors, handleSubmit, control, watch } = useForm({
+        validationSchema,
+        reValidateMode: "onSubmit",
+    });
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (authUser) return router.push("/library");
+    }, [authUser]);
+
+    const dialCode = (countryCode) => {
+        const country = dialCodes.find((country) => country.code === countryCode);
+
+        return get(country, "dialCode", null);
+    };
+
+    const signUpUser = async (user) => {
+        await signUp({
+            ...user,
+            birthDate: moment(
+                `${user.day}/${user.month}/${user.year}`,
+                "DD/MM/YYYY"
+            ).toDate(),
+        });
+    };
+
+    return (
+        <RegisterContainer>
+            <Desktop>
+                <Image src={`${config.storageUrl}/resoruces/ebombo-white.png`} />
+            </Desktop>
+
+            <form onSubmit={handleSubmit(signUpUser)} autoComplete="off" noValidate>
+                <Divider>
+                    <div className="divider-content">
+                        <Image
+                            margin="0"
+                            width="15px"
+                            height="15px"
+                            src={`${config.storageUrl}/resources/personal-info.svg`}
+                            size="contain"
+                        />
+                        <div className="subtitle">Información Personal</div>
+                    </div>
+                </Divider>
+                <div className="inputs-container">
+                    <Input
+                        error={errors.name}
+                        type="text"
+                        ref={register}
+                        name="name"
+                        autoComplete="off"
+                        placeholder="Nombre"
+                    />
+                </div>
+                <div className="inputs-container">
+                    <Input
+                        error={errors.lastName}
+                        type="text"
+                        ref={register}
+                        name="lastName"
+                        autoComplete="off"
+                        placeholder="Apellidos"
+                    />
+                </div>
+                <div className="inputs-container">
+                    <div className="label">Fecha de Nacimiento</div>
+                    <InputGroupSelect>
+                        <Controller
+                            name="day"
+                            control={control}
+                            as={
+                                <Select
+                                    placeholder="día"
+                                    showSearch
+                                    virtual={false}
+                                    error={errors.day}
+                                    borderRight={`0.1px solid ${darkTheme.basic.grayLighten}`}
+                                    optionFilterProp="children"
+                                    borderRadius="3px 0px 0px 3px"
+                                    optionsdom={days.map((day) => ({
+                                        key: day.key,
+                                        name: day.value,
+                                        code: day.value,
+                                    }))}
+                                />
+                            }
+                        />
+                        <Controller
+                            name="month"
+                            control={control}
+                            as={
+                                <Select
+                                    placeholder="mes"
+                                    showSearch
+                                    virtual={false}
+                                    error={errors.month}
+                                    borderRight={`0.1px solid ${darkTheme.basic.grayLighten}`}
+                                    borderRadius="0px 0px 0px 0px"
+                                    optionFilterProp="children"
+                                    optionsdom={months.map((month) => ({
+                                        key: month.key,
+                                        code: month.key,
+                                        name: month.value,
+                                    }))}
+                                />
+                            }
+                        />
+                        <Controller
+                            name="year"
+                            control={control}
+                            as={
+                                <Select
+                                    placeholder="año"
+                                    showSearch
+                                    virtual={false}
+                                    borderRadius="0px 3px 3px 0px"
+                                    error={errors.year}
+                                    optionFilterProp="children"
+                                    optionsdom={years.map((year) => ({
+                                        key: year.key,
+                                        name: year.value,
+                                        code: year.value,
+                                    }))}
+                                />
+                            }
+                        />
+                    </InputGroupSelect>
+                </div>
+
+                <Divider>
+                    <div className="divider-content">
+                        <Image
+                            margin="0"
+                            width="15px"
+                            height="15px"
+                            src={`${config.storageUrl}/resources/contact-info.svg`}
+                            size="contain"
+                        />
+                        <div className="subtitle">Contacto</div>
+                    </div>
+                </Divider>
+                <div className="inputs-container">
+                    <Input
+                        error={errors.email}
+                        type="email"
+                        ref={register}
+                        name="email"
+                        autoComplete="off"
+                        placeholder="Correo"
+                    />
+                </div>
+                <div className="inputs-container">
+                    <InputGroup gridTemplateColumns="2fr 50px 3fr">
+                        <Controller
+                            name="countryCode"
+                            control={control}
+                            as={
+                                <Select
+                                    placeholder="Pais"
+                                    showSearch
+                                    virtual={false}
+                                    error={errors.countryCode}
+                                    optionFilterProp="children"
+                                    optionsdom={getData().map((country) => ({
+                                        key: country.code,
+                                        code: country.code,
+                                        name: country.name,
+                                    }))}
+                                />
+                            }
+                        />
+                        <CountryCode>{dialCode(watch("countryCode"))}</CountryCode>
+                        <Input
+                            error={errors.phoneNumber}
+                            type="number"
+                            ref={register}
+                            name="phoneNumber"
+                            autoComplete="off"
+                            placeholder="Celular"
+                        />
+                    </InputGroup>
+                </div>
+                <Divider>
+                    <div className="divider-content">
+                        <Image
+                            margin="0"
+                            width="15px"
+                            height="15px"
+                            src={`${config.storageUrl}/resources/access-data.svg`}
+                            size="contain"
+                        />
+                        <div className="subtitle">Datos de acceso</div>
+                    </div>
+                </Divider>
+                <div className="inputs-container">
+                    <Input
+                        error={errors.password}
+                        type="password"
+                        ref={register}
+                        name="password"
+                        autoComplete="off"
+                        placeholder="Contraseña"
+                    />
+                    <ButtonAnt
+                        loading={isLoadingCreateUser}
+                        disabled={isLoadingUser || isLoadingCreateUser}
+                        variant="contained"
+                        htmlType="submit"
+                        width="100%"
+                        margin="0.5rem 0"
+                    >
+                        Registrar
           </ButtonAnt>
-        </div>
-      </form>
-      <Divider>
-        <div className="divider-content">O</div>
-      </Divider>
-      <div className="inputs-container">
-        <ButtonsProviders google />
-      </div>
-    </RegisterContainer>
-  );
+                </div>
+            </form>
+            <Divider>
+                <div className="divider-content">O</div>
+            </Divider>
+            <div className="inputs-container">
+                <ButtonsProviders google />
+            </div>
+        </RegisterContainer>
+    );
 };
 
 const RegisterContainer = styled.div`
   margin: 2rem auto;
   background-color: ${(props) => props.theme.basic.gray};
   min-height: 100vh;
+  display:grid;
+
+  ${mediaQuery.afterDesktop}{
+      grid-template-columns:1fr 1fr;
+  }
 
   .title {
     color: ${(props) => props.theme.basic.white};
