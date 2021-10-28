a1e99402b07cfb583b459297ff33a0d7694b9363
anargu <anthony.arostegui@gmail.com>
2021-10-28 16:32:45 -0500

    fix removed hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 export * from "./useLocalStorageState";
 export * from "./useUploadToStorage";
 export * from "./useResizeImage";
-export * from "./useWindowScreen";
 export * from "./useSendError";
 export * from "./acl";
