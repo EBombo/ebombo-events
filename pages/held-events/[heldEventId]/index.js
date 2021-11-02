7de6e5ccdcef85e3602261c26baec3e75d455b40
anargu <anthony.arostegui@gmail.com>
2021-11-02 17:37:31 -0500

    refactored imports

--- pages/held-events/[heldEventId]/index.js
+++ pages/held-events/[heldEventId]/index.js
@@ -1,13 +1,11 @@
 import React from "reactn";
 import { SEOMeta } from "../../../src/components/common/seo";
-//TODO: It should be called HeldEventDetails as parent folder.
-import { UseCaseDetail } from "../../../src/pages/held-events/_heldEventId";
+import { HeldEventDetails } from "../../../src/pages/held-events/_heldEventId";
 
 const Init = (props) => (
   <>
     <SEOMeta {...props} />
-    {/*TODO: It should be called HeldEventDetails as parent folder.*/}
-    <UseCaseDetail {...props} />
+    <HeldEventDetails {...props} />
   </>
 );
 
--- pages/held-events/index.js
+++ pages/held-events/index.js
@@ -1,13 +1,11 @@
 import React from "reactn";
-// TODO: It should export HeldEvents [as the route] from "../../src/pages/held-events" not useCases.
-import { UseCases } from "../../src/pages/held-events";
+import { HeldEvents } from "../../src/pages/held-events";
 import { SEOMeta } from "../../src/components/common/seo";
 
 const Init = (props) => (
   <>
     <SEOMeta {...props} />
-    {/*TODO: It should be called HeldEvents.*/}
-    <UseCases {...props} />
+    <HeldEvents {...props} />
   </>
 );
 
