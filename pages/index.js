62940bb9350de2d5b28fd9ea9c01956a2cc35167
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-11-09 18:22:06 -0500

    using nav as wrap

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from "reactn";
 import { Home } from "../src/pages/home";
 import { SEOMeta } from "../src/components/common/seo";
+import { Navbar } from "../src/components/Navbar";
 
 const Init = (props) => (
   <>
     <SEOMeta {...props} />
-    <Home {...props} />
+    <Navbar>
+      <Home {...props} />
+    </Navbar>
   </>
 );
 
