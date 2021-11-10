91477b2738efc4f9dc8b894a0931f371917a806d
anargu <anthony.arostegui@gmail.com>
2021-11-10 12:11:12 -0500

    updated data

--- src/components/common/DataList.js
+++ src/components/common/DataList.js
@@ -142,24 +142,53 @@ export const plans = [
   },
 ];
 
+export const products = [
+  {
+    id: "eventos-virtuales",
+    title: "Eventos Virtuales",
+    imageUrl: "https://via.placeholder.com/274x130",
+    content: [
+      "Hemos hecho más de 200 eventos virtuales de todo tipo cómo:",
+      (
+        <ul>
+          <li>Integraciones con juegos</li>
+          <li>Speakers Motivacionales</li>
+          <li>Olimpiadas</li>
+          <li>Eventos temáticos</li>
+          <li>Deportes virtuales</li>
+          <li>Cuenta cuentos</li>
+          <li>Muchos más</li>
+        </ul>
+      ),
+      "Nos adecuamos a las necesidades de cada empresa. Sabemos que cada organización tiene su propia cultura y clima laboral. Dinos que necesitas y lo haremos realidad.",
+    ],
+  },
+  {
+    id: "eventos-presenciales",
+    title: "Eventos Presenciales",
+    imageUrl: "https://via.placeholder.com/274x130",
+    content: [
+      "A",
+    ],
+  },
+  {
+    id: "juegos-de-integracion",
+    title: "Juegos de integración",
+    image: "https://via.placeholder.com/274x130",
+    content: [
+      "A",
+    ],
+  }
+];
+
 export const infoGamesData = [
   {
     id: "bingo",
     menuLabel: "Bingo",
-    sections: [
-      {
-        title: "Bingo",
-        imageUrl: "https://via.placeholder.com/274x130",
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re`,
-      },
-      {
-        title: "Bingo",
-        imageUrl: "https://via.placeholder.com/274x130",
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re`,
-      }
-    ]
+    title: "Bingo",
+    imageUrl: "https://via.placeholder.com/274x130",
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+ It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more re`,
   },
 ];
 
