37dda25767c2b0eff401567f58b49a4f36646cb0
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-11-15 10:18:18 -0500

    add TODO

--- src/pages/games/_gameId/index.js
+++ src/pages/games/_gameId/index.js
@@ -19,6 +19,7 @@ export const GameDetail = (props) => {
         <div className="back-container">
           <Icon className="back-icon" type="left" onClick={() => router.back()} />
         </div>
+        {/*TODO: Consider refactoring, move tyle from <GameInfoSectionsContainer> into <GameInfoSection>*/}
         <GameInfoSectionsContainer>{infoGame && <GameInfoSection infoGame={infoGame} />}</GameInfoSectionsContainer>
       </div>
     </LandingContainer>
