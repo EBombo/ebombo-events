8c61b32c4cd90b65252e0e74345c8084f71a77ca
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-11-29 13:12:16 -0500

    add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -145,7 +145,16 @@ const MyApp = ({ Component, pageProps }) => {
         <Script
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: ``,
+            __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2724726,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
           }}
         />
       </Head>
