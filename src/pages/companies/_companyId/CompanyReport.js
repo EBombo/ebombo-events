6268f035b7a7ac1deb3e32f9cbaf45497a0929bf
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-12-03 13:10:41 -0500

    create report

---
+++ src/pages/companies/_companyId/CompanyReport.js
@@ -0,0 +1,8 @@
+import React from "reactn";
+import styled from "styled-components";
+
+export const CompanyReport = (props) => {
+  return <ReportStyled>hola</ReportStyled>;
+};
+
+const ReportStyled = styled.div``;
