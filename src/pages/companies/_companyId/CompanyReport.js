d0cc1d8a2a301d10984b16cbe10be5e681e97b4a
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-12-03 21:55:21 -0500

    create report v1

--- src/pages/companies/_companyId/CompanyReport.js
+++ src/pages/companies/_companyId/CompanyReport.js
@@ -1,8 +1,86 @@
 import React from "reactn";
 import styled from "styled-components";
+import moment from "moment";
 
 export const CompanyReport = (props) => {
-  return <ReportStyled>hola</ReportStyled>;
+  return (
+    <ReportStyled>
+      <div className="last-update">Última actualización: {moment().format("LLL")}</div>
+
+      <div className="control-dates">
+        <div className="date">
+          <div className="title">Desde</div>
+          <div className=""></div>
+        </div>
+        <div className="date">
+          <div className="title">Hasta</div>
+          <div className=""></div>
+        </div>
+      </div>
+
+      <div className="metrics">
+        <div className="head">
+          <div className="">Juegos</div>
+          <div className="">?</div>
+        </div>
+        <div className="body">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+
+      <div className="metrics">
+        <div className="head">
+          <div className="">Usuarios</div>
+          <div className="">?</div>
+        </div>
+        <div className="body">
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+
+      <div className="list-users">
+        <div className="title">Anfitriones principales</div>
+        <div className="head">
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+        <div className="body">
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </div>
+      </div>
+    </ReportStyled>
+  );
 };
 
-const ReportStyled = styled.div``;
+const ReportStyled = styled.div`
+  .last-update {
+  }
+
+  .control-dates {
+    .date {
+      .title {
+      }
+    }
+  }
+
+  .metrics {
+    .head {
+    }
+    .body {
+    }
+  }
+
+  .list-users {
+    .title {
+    }
+    .head {
+    }
+    .body {
+    }
+  }
+`;
