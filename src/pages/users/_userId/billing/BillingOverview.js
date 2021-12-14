6f655cb4397a0152e42785ddbf586533ce1fd41f
anargu <anthony.arostegui@gmail.com>
2021-12-14 18:01:50 -0500

    updated billing overview

--- src/pages/users/_userId/billing/BillingOverview.js
+++ src/pages/users/_userId/billing/BillingOverview.js
@@ -1,130 +1,214 @@
-import React, { useGlobal, useState } from "reactn";
+import React, { useGlobal, useState, useEffect } from "reactn";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import { DownloadOutlined } from "@ant-design/icons";
 import { Table, Space } from "antd";
+import moment from "moment";
 import { mediaQuery, Desktop, Tablet } from "../../../../constants";
 import { PanelBox } from "../../../../components/common/PanelBox";
+import { stripeDateFormat, getTypePaymentPrice, getCurrencySymbol, BrandCardIcon } from "../../../../components/common/DataList";
 import { ModalContainer } from "../../../../components/common/ModalContainer";
+import { Image } from "../../../../components/common/Image";
 import { Anchor, ButtonAnt } from "../../../../components/form";
+import { spinLoader, spinLoaderMin } from "../../../../components/common/loader";
 import { InvoiceTable } from "../invoices/InvoiceTable";
 import { darkTheme } from "../../../../theme";
-import { goToPortalLink } from "../../../../stripe";
+import { formatAmount, goToPortalLink } from "../../../../stripe";
+import { firestore } from "../../../../firebase";
+import { snapshotToArray } from "../../../../utils";
+import { useSendError } from "../../../../hooks";
 
 export const BillingOverview = (props) => {
-  const [isVisibleCancelSubscription, setIsVisibleCancelSubscription] = useState(false);
+  const { sendError } = useSendError();
+
+  const router = useRouter();
+  const { userId, subscriptionId } = router.query;
+
+  const [isLodingPortalLink, setIsLoadingPortalLink] = useState(false);
+  const [subscription, setSubscription] = useState();
+  const [invoice, setInvoice] = useState();
+  const [paymentInformation, setPaymentInformation] = useState();
+
+  const fetchUserSubscription = () => firestore.collection(`customers/${userId}/subscriptions`).doc(subscriptionId).get();
+
+  const fetchLastInvoice = () => firestore.collection(`customers/${userId}/subscriptions/${subscriptionId}/invoices`).orderBy('created', 'desc').limit(1).get();
+  const fetchPaymentInformation = (paymentIntent) => firestore.collection(`customers/${userId}/payments`).doc(paymentIntent).get();
+
+  useEffect(() => {
+    if (subscription) return;
+
+    const getSubscription = async () => {
+      const _subscription = (await fetchUserSubscription()).data();
+
+      setSubscription(_subscription);
+    };
+
+
+    return getSubscription();
+  }, []);
+
+  useEffect(() => {
+    if (invoice) return;
+
+    const getInvoice = async () => {
+      const invoices = snapshotToArray(await fetchLastInvoice());
+      const _invoice = invoices?.[0];
+      setInvoice(_invoice);
+
+      const _paymentInformation = (await fetchPaymentInformation(_invoice['payment_intent'])).data();
+      setPaymentInformation(_paymentInformation);
+    };
+
+    return getInvoice();
+  }, []);
+
+  const goToCustomerPortal = async () => {
+    setIsLoadingPortalLink(true);
+    try {
+      await goToPortalLink();
+    } catch (err) {
+      setIsLoadingPortalLink(false);
+      sendError(err);
+      props.showNotification('Error', 'Algo salió mal, intente dentro de unos momentos', 'error');
+    }
+  };
+
+  if (!subscription) return spinLoader();
 
   return (
     <BillingDetailContainer>
       <ModalContainer
-        background={darkTheme.basic.gray}
-        visible={isVisibleCancelSubscription}
+        background={darkTheme.basic.whiteLight}
+        visible={isLodingPortalLink}
         footer={null}
         closable
         padding={"1rem"}
-        onCancel={() => setIsVisibleCancelSubscription(false)}
+        onCancel={() => setIsLoadingPortalLink(false)}
       >
-        <div>¿Cancelar Suscripción?</div>
-        <div>Si se cancela, la suscripción continuará hasta la próxima fecha de facturación y luego caducará.</div>
-        <div className="table">
-          <ButtonAnt onClick={() => setIsVisibleCancelSubscription}>Cerrar</ButtonAnt>
-          <ButtonAnt onClick={() => goToPortalLink()}>Cancelar suscripción</ButtonAnt>
-        </div>
-
-        
+        <LoadingPortalLinkStyled>
+          <div>Se está redireccionando a la gestión de su plan</div>
+          <div>{spinLoaderMin()}</div>
+          <div className="table">
+            <ButtonAnt onClick={() => setIsLoadingPortalLink(false)}>Aceptar</ButtonAnt>
+          </div>
+        </LoadingPortalLinkStyled>
       </ModalContainer>
 
       <div className="section">
         <h2 className="title">Suscripciones</h2>
         <div className="subscriptions-layout">
-          <PanelBox className="balance" elevated>
-            <div className="table">
-              <div className="label">Saldo actual de la cuenta</div>
-              <div className="value">-</div>
-            </div>
-          </PanelBox>
 
-          <PanelBox className="plan" heading="Plan Anual Billing" elevated>
+          <PanelBox className="plan" heading="Plan Anual Billing" elevated margin="0 0 1rem 0">
             <div className="table">
               <div className="label">Periodo actual</div>
-              <div className="value">-</div>
+              <div className="value">{ moment(subscription?.current_period_start.toDate()).format(stripeDateFormat) } - { moment(subscription?.current_period_end.toDate()).format(stripeDateFormat) }</div>
 
               <div className="label">Comportamiento de término</div>
-              <div className="value">-</div>
-
-              <div className="label">Colección</div>
-              <div className="value">-</div>
+              <div className="value">{ getTypePaymentPrice[subscription?.items?.[0].price.type] }</div>
 
               <div className="label">Se renueva en</div>
-              <div className="value">-</div>
+              <div className="value">{ moment(subscription?.current_period_end.toDate()).format(stripeDateFormat) }</div>
 
               <div className="label">Comenzó en</div>
-              <div className="value">-</div>
+              <div className="value">{ moment(subscription?.current_period_start.toDate()).format(stripeDateFormat) }</div>
 
-              <div className="label">Últimas Factura</div>
-              <div className="value">-</div>
-            </div>
+              <div className="label">Última Factura</div>
+              <div className="value">
+                <Anchor 
+                  variant="primary"
+                  underlined
+                  onClick={() => router.push(`/users/${userId}/invoices/${invoice?.id}?subscriptionId=${subscriptionId}`)}
+                >#{ invoice?.number }</Anchor></div>
+              </div>
 
             <div className="alert accent">
-              <p className="description">Siguiente factura en </p>
+              <p className="description">Siguiente factura en {moment(subscription?.current_period_end.toDate()).format(stripeDateFormat)   }</p>
               <div className="table">
-                <div className="label">Plan -</div>
-                <div className="value">-</div>
+                <div className="label">{ subscription?.items?.[0].price?.product.name }</div>
+                <div className="value">{ formatAmount(subscription?.items?.[0].plan?.amount) } { getCurrencySymbol[subscription?.items?.[0].plan?.currency] }</div>
               </div>
               <hr className="divider"/>
               <div className="table">
                 <div className="label">Total Actual</div>
-                <div className="value">-</div>
+                <div className="value">{ formatAmount(subscription?.items?.[0].plan?.amount) } { getCurrencySymbol[subscription?.items?.[0].plan?.currency] }</div>
               </div>
 
             </div>
             <div className="right">
-              <Anchor variant="primary" onClick={() => {
-                setIsVisibleCancelSubscription(true);
-              }}>Cancelar suscripción</Anchor>
+              <Anchor 
+                variant="primary"
+                onClick={() => goToCustomerPortal()}
+              >
+                Cancelar suscripción
+              </Anchor>
             </div>
           </PanelBox>
 
           <PanelBox
-           className="billing-detail"
+            className="billing-detail"
             elevated
             heading="Datos de facturación"
-            actionLink={<Anchor onClick={() => {}}>Revisar</Anchor>}
+            margin="0 0 1rem 0"
           >
-            <div className="table">
-              <div className="label">Nombre</div>
-              <div className="value">-</div>
+            { !paymentInformation
+              ? spinLoader()
+              : (
+                <div className="table">
+                  <div className="label">Nombre</div>
+                  <div className="value">{ paymentInformation?.charges.data?.[0]?.billing_details?.name }</div>
 
-              <div className="label">Tarjeta de Crédito</div>
-              <div className="value">-</div>
+                  <div className="label">Tarjeta de Crédito</div>
+                  <div className="value">
+                    <Image
+                      className="brand-logo"
+                      src={ BrandCardIcon[paymentInformation?.charges.data?.[0]?.payment_method_details?.card?.brand] }
+                      width="32px"
+                    /> &bull; { paymentInformation?.charges.data?.[0]?.payment_method_details?.card?.last4 }
+                  </div>
 
-              <div className="label">Caducidad</div>
-              <div className="value">-</div>
+                  <div className="label">Caducidad</div>
+                  <div className="value">{ paymentInformation?.charges.data?.[0]?.payment_method_details?.card?.exp_month } / { paymentInformation?.charges.data?.[0]?.payment_method_details?.card?.exp_year }</div>
 
-              <div className="label">Dirección</div>
-              <div className="value"> - </div>
-            </div>
+                  <div className="label">Dirección</div>
+                  <div className="value">{ paymentInformation?.charges.data?.[0]?.billing_details?.address?.postal_code } { paymentInformation?.charges.data?.[0]?.billing_details?.address?.country }</div>
+                </div>
+              )
+            }
+            
           </PanelBox>
         </div>
 
         <div className="table">
           <h2 className="title">Facturas</h2>
-          <Anchor className="action-link" onClick={() => {}}>Ver todas las facturas</Anchor>
+          <Anchor
+            className="action-link"
+            variant="primary"
+            onClick={() => router.push(`/users/${userId}/invoices?subscriptionId=${subscriptionId}`)}
+          >Ver todas las facturas</Anchor>
         </div>
 
-        <InvoiceTable {...props} />
+        <InvoiceTable {...props} userId={userId} subscriptionId={subscriptionId}/>
       </div>
 
     </BillingDetailContainer>
   );
 };
 
+const LoadingPortalLinkStyled = styled.div`
+  color: ${(props) => props.theme.basic.black};
+  font-size: 1rem;
+`;
+
 const BillingDetailContainer = styled.div`
+  .brand-logo {
+    display: inline-block;
+  }
+
   .section {
-    margin: 0 8px;
+    margin: 12px 8px 0 8px;
     ${mediaQuery.afterTablet} {
       max-width: 1200px;
-      margin: 0 auto;
+      margin: 12px auto 0 auto;
     }
   }
 
@@ -160,31 +244,6 @@ const BillingDetailContainer = styled.div`
     border-color: ${(props) => props.theme.basic.grayLighten};
   }
 
-  .table-billing {
-    border-collapse: collapse;
-
-    thead {
-      border: 2px solid ${(props) => props.theme.basic.grayLighten};
-    }
-
-    th {
-      background: ${(props) => props.theme.basic.whiteDark};
-      border-bottom: 1px solid ${(props) => props.theme.basic.grayLighten};
-      border-top: 1px solid ${(props) => props.theme.basic.grayLighten};
-
-      &:first-child {
-        border-left: 1px solid ${(props) => props.theme.basic.grayLighten};
-        border-top-left-radius: 5px !important;
-        border-bottom-left-radius: 5px !important;
-      }
-      &:last-child {
-        border-right: 1px solid ${(props) => props.theme.basic.grayLighten};
-        border-top-right-radius: 5px !important;
-        border-bottom-right-radius: 5px !important;
-      }
-    }
-  }
-
   ${mediaQuery.afterTablet} {
     .subscriptions-layout {
       display: grid;
@@ -196,16 +255,9 @@ const BillingDetailContainer = styled.div`
         grid-column: 1 / 2;
       }
 
-      .balance {
-        grid-row: 1 / 2;
-        grid-column: 2 / 3;
-        margin-bottom: 0;
-      }
-
       .billing-detail {
-        grid-row: 2 / 3;
+        grid-row: 1 / 3;
         grid-column: 2 / 3;
-        margin-top: 0;
       }
     }
   }
