d87dfb3dc6cbe726a44d6d68058e0c31f284388c
Pablo <tech@ebombo.com>
2022-02-14 18:51:12 +0000

    add cloud shell support

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -22,7 +22,7 @@ const config = JSON.parse(CONFIG);
 
 const hostName = typeof window === "undefined" ? DOMAIN : window.location.hostname;
 
-if (DOMAIN?.includes("local") || DOMAIN?.includes("red") || DOMAIN?.includes("dev")) {
+if (DOMAIN?.includes("local") || DOMAIN?.includes("red") || DOMAIN?.includes("dev") || DOMAIN?.includes("shell")) {
   console.log("dev", version);
 } else {
   console.log("prod", version);
