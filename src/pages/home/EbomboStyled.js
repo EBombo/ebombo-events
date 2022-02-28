310deb8557bb7492200ea7566bd297d4c878a416
Pablo <pablo.yncio@immertec.com>
2022-02-28 18:36:18 -0500

    add items landing

--- src/pages/home/EbomboStyled.js
+++ src/pages/home/EbomboStyled.js
@@ -1,58 +1,70 @@
 import React from "reactn";
 import styled from "styled-components";
+import { Image } from "../../components/common/Image";
+import { config } from "../../firebase";
+import { mediaQuery } from "../../constants";
 
 const EbomboItems = [
   {
     title: "Happy Hour",
     description:
       "Reúne a tu equipo en una happy hour virtual y olvídate de la distancia. Diversión asegurada con o sin bebidas, tú decides.",
-    img: "",
+    img: "happy-hour.png",
+    color: "#FFD15B",
   },
   {
     title: "Días festivos",
     description:
       "Ya sea estés celebrando Navidad, el día de la madre, halloween, el día de los muertos, el día del minero o cualquier día festivo, tenemos experiencia para celebrar estas fechas.",
-    img: "",
+    img: "dias-festivos.png",
+    color: "#E3954E",
   },
   {
     title: "Actividades rompe hielo",
     description:
       "Tenemos muchas actividades para fortalecer el clima laboral en la empresa. Realiza divertidas dinámicas para motivar tu equipo   y ayudar a tus compañeros a entenderse más entre ellos. ",
-    img: "",
+    img: "hielo.png",
+    color: "#93D8E4",
   },
   {
     title: "Eventos corporativos",
     description:
       "ebombo crea inolvidables experiencias y momentos íntimos para ti y tus compañeros donde todos puedan ser parte de las actividades y jugar desde su celular nuestros juegos.",
-    img: "",
+    img: "corporativos.png",
+    color: "#E12AFF",
   },
   {
     title: "Reuniones",
     description: "Dale ese toque divertido a tus reuniones de siempre con nuestras actividades virtuales. ",
-    img: "",
+    img: "reuniones.png",
+    color: "#7AFFE4",
   },
   {
     title: "Training y Orientación",
     description:
       "Nada como jugar para aprender actividades y tareas importantes. Empieza con el pie derecho con los nuevos contratados, enséñales todo lo que necesitan saber, involúcralos y ténlos enganchados.",
-    img: "",
+    img: "orientacion.png",
+    color: "#4FBA6F",
   },
   {
     title: "Conecta",
     description:
       "Conectar a tu equipo de manera virtual no tiene porque ser un desafío. Hemos construído actividades innovadoras con tecnología que une personas en cualquier lugar, en cualquier momento y en cualquier ocasión.",
-    img: "",
+    img: "conecta.png",
+    color: "#98B3F9",
   },
   {
     title: "Reconocimientos",
     description:
       "Reconoce y premia a tus empleados en un divertido y entretenido evento. Mantén a tus participantes enganchados de una manera que otra plataforma no puede.",
-    img: "",
+    img: "reconocimiento.png",
+    color: "#EE4541",
   },
   {
     title: "Actividades de clima",
     description: "Realiza actividades para tu equipo de una forma divertida, auténtica e innovadora.",
-    img: "",
+    img: "clima.png",
+    color: "#74829C",
   },
 ];
 
@@ -64,11 +76,17 @@ export const EbomboStyle = (props) => {
       <div className="items">
         {EbomboItems.map((item) => {
           return (
-            <div className="item">
+            <ItemStyled color={item.color}>
               <div className="title-item">{item.title}</div>
               <div className="description-item">{item.description}</div>
-              <div>img</div>
-            </div>
+              <Image
+                src={`${config.storageUrl}/resources/ebombo-style/${item.img}`}
+                width="150px"
+                height="150px"
+                size="content"
+                margin="0"
+              />
+            </ItemStyled>
           );
         })}
       </div>
@@ -76,18 +94,38 @@ export const EbomboStyle = (props) => {
   );
 };
 
+const ItemStyled = styled.div`
+  padding: 10px 20px;
+  border-radius: 5px;
+  border-top: 5px solid ${(props) => props.color};
+  background: ${(props) => props.theme.basic.white};
+
+  .title-item {
+    margin: 10px 0;
+    font-weight: bold;
+    color: ${(props) => props.theme.basic.primary};
+  }
+`;
+
 const EbomboStyleStyled = styled.div`
   padding: 1rem 2rem;
   background: linear-gradient(276.15deg, #331e6d 0%, #6646b7 100%);
+  padding-bottom: 1rem;
 
   .title {
-    font-size: xx-large;
+    margin: 2rem 0;
     text-align: center;
+    font-size: xx-large;
     color: ${(props) => props.theme.basic.white};
   }
 
   .items {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 25px;
+    grid-template-columns: 1fr;
+
+    ${mediaQuery.afterTablet} {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
   }
 `;
