7db251d24cf5254065ad51a04f0455575796941e
Pablo <pablo.yncio@immertec.com>
2022-03-28 10:52:38 -0500

    date event

--- src/pages/events/eventId/DatesEvent.js
+++ src/pages/events/eventId/DatesEvent.js
@@ -5,7 +5,7 @@ import { Image } from "../../../components/common/Image";
 import { TimePicker } from "antd";
 
 export const DatesEvent = (props) => {
-  const [currentDate, setCurrentDate] = useState({ id: firestore.collection("event").doc().id });
+  const [currentDate, setCurrentDate] = useState({});
 
   return (
     <div>
@@ -18,17 +18,17 @@ export const DatesEvent = (props) => {
       <div className="grid grid-cols-4 gap-3 w-9/12 rounded-md border-2 border-grayLighten py-6 px-4 mb-9">
         <div>
           <div className="text-secondary mb-4">Día del evento</div>
-          <DatePicker format="ll" onChange={(event) => setCurrentDate({ month: event })} />
+          <DatePicker format="ll" onChange={(event) => setCurrentDate({ ...currentDate, month: event })} />
         </div>
 
         <div>
           <div className="text-secondary mb-4">Inicio del evento</div>
-          <TimePicker className="w-full" onChange={(event) => setCurrentDate({ start: event })} />
+          <TimePicker className="w-full" onChange={(event) => setCurrentDate({ ...currentDate, start: event })} />
         </div>
 
         <div>
           <div className="text-secondary mb-4">Fin del evento</div>
-          <TimePicker className="w-full" onChange={(event) => setCurrentDate({ end: event })} />
+          <TimePicker className="w-full" onChange={(event) => setCurrentDate({ ...currentDate, end: event })} />
         </div>
 
         <div className="flex">
@@ -37,7 +37,9 @@ export const DatesEvent = (props) => {
             color="primary"
             variant="contained"
             disabled={!currentDate?.month && !currentDate?.start && !currentDate?.end}
-            onClick={() => props.setDates([currentDate])}
+            onClick={() =>
+              props.setDates([...(props.dates ?? []), { id: firestore.collection("event").doc().id, ...currentDate }])
+            }
           >
             Adicionar fecha
           </ButtonAnt>
@@ -48,42 +50,40 @@ export const DatesEvent = (props) => {
         <>
           <div className="text-secondary mb-4 text-xl">Fechas tentativas elegidas</div>
 
-          {props.dates.map((date) => {
-            return (
-              <div
-                className="grid grid-cols-5 gap-3 w-9/12 rounded-md border-2 border-grayLighten py-6 px-4"
-                key={date.id}
-              >
-                <div>
-                  <div className="text-grayDarken mb-4">Día del evento</div>
-                  <div className="text-secondary text-base mb-4">Fecha...</div>
-                </div>
-
-                <div>
-                  <div className="text-grayDarken mb-4">Inicio del evento</div>
-                  <div className="text-secondary text-base mb-4">Fecha...</div>
-                </div>
-
-                <div>
-                  <div className="text-grayDarken mb-4">Fin del evento</div>
-                  <div className="text-secondary text-base mb-4">Fecha...</div>
-                </div>
-
-                <Anchor underlined margin="auto 0" variant="secondary">
-                  Editar
-                </Anchor>
-
-                <Image
-                  src={`${config.storageUrl}/resources/delete.svg`}
-                  width="16px"
-                  height="16px"
-                  size="contain"
-                  margin="auto"
-                  cursor="pointer"
-                />
+          {props.dates.map((date) => (
+            <div
+              className="grid grid-cols-5 gap-3 w-9/12 rounded-md border-2 border-grayLighten py-6 px-4 mb-4"
+              key={date.id}
+            >
+              <div>
+                <div className="text-grayDarken mb-4">Día del evento</div>
+                <div className="text-secondary text-base mb-4">{date.month.format("LLL")}</div>
               </div>
-            );
-          })}
+
+              <div>
+                <div className="text-grayDarken mb-4">Inicio del evento</div>
+                <div className="text-secondary text-base mb-4">Fecha...</div>
+              </div>
+
+              <div>
+                <div className="text-grayDarken mb-4">Fin del evento</div>
+                <div className="text-secondary text-base mb-4">Fecha...</div>
+              </div>
+
+              <Anchor underlined margin="auto 0" variant="secondary">
+                Editar
+              </Anchor>
+
+              <Image
+                src={`${config.storageUrl}/resources/delete.svg`}
+                width="16px"
+                height="16px"
+                size="contain"
+                margin="auto"
+                cursor="pointer"
+              />
+            </div>
+          ))}
         </>
       ) : null}
 
