7c5428c536bc1216d366c494cebf403bdb571048
Pablo <pablo.yncio@immertec.com>
2022-04-05 23:48:02 -0500

    optimization

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -1,4 +1,5 @@
 import get from "lodash/get";
+import { useCallback } from "reactn";
 import { useRouter } from "next/router";
 import en from "../../public/locales/en.json";
 import es from "../../public/locales/es.json";
@@ -14,11 +15,26 @@ export const useTranslation = (path) => {
   const locales = Object.keys(TRANSLATIONS);
 
   // Update language and redirect.
-  const setLocale = (locale) => router.push(asPath, asPath, { locale });
+  const setLocale = useCallback(
+    (locale) => {
+      router.push(asPath, asPath, { locale });
+    },
+    [asPath]
+  );
 
   // You can use:
   // const {t} = useTranslation("landing") or t("landing.title")
-  const t = (keyString) => get(TRANSLATIONS[locale], `${path ? `${path}.` : ""}${keyString}`);
+  const t = useCallback(
+    (keyString) => {
+      return get(TRANSLATIONS[locale], `${path ? `${path}.` : ""}${keyString}`);
+    },
+    [TRANSLATIONS, locale]
+  );
 
   return { t, locales, locale, setLocale };
 };
+
+// References:
+// https://betterprogramming.pub/build-your-own-usetranslation-hook-with-next-js-2c65017d323a
+// https://nextjs.org/docs/advanced-features/i18n-routing#limits-for-the-i18n-config
+// https://github.com/a-chris/use-translation-example
