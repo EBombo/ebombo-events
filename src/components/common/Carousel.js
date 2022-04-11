9eab5e8f6f2e5f1072dfabb85fb3f9e289d422b7
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-04-11 14:00:20 -0500

    fixing carousel

--- src/components/common/Carousel.js
+++ src/components/common/Carousel.js
@@ -1,12 +1,12 @@
-import React, { useRef, useState, forwardRef } from "react";
+import React, { useRef, useState } from "react";
 import { Carousel as CarouselAntd } from "antd";
 import styled from "styled-components";
 import { Arrows } from "./arrowsCarousel";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { ButtonAnt } from "../form";
 
-export const Carousel = forwardRef((props, ref) => {
-  const slider = useRef(ref);
+export const Carousel = (props) => {
+  const slider = useRef(null);
   const [indicator, setIndicator] = useState(0);
   const lengthComponents = props.components.length;
 
@@ -45,8 +45,8 @@ export const Carousel = forwardRef((props, ref) => {
 
       <CarouselStyled
         effect="fade"
-        ref={ref}
-        dots={props.dots ?? false}
+        ref={slider}
+        dots={false}
         autoplay={!!props.autoplay}
         afterChange={(current) => setIndicator(current)}
         width={props.width}
@@ -69,7 +69,7 @@ export const Carousel = forwardRef((props, ref) => {
       {!props.hideIndicators && <Arrows next={next} prev={prev} indicator={indicator} goTo={goTo} {...props} />}
     </Container>
   );
-});
+};
 
 const Container = styled.div`
   width: ${(props) => (props.width ? props.width : "100%")};
