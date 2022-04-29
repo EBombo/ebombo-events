1116c5f15c635b5024185041f1de6ec28740976e
Pablo <pablo.yncio@immertec.com>
2022-04-29 16:06:37 -0500

    remove report tab

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "reactn";
+import React, { useState } from "reactn";
 import dynamic from "next/dynamic";
 import { spinLoader } from "../../src/components/common/loader";
 import { PrivateRoutes } from "../../src/routes/PrivateRoutes";
 import { SEOMeta } from "../../src/components/common/seo";
-import { CompanyReport } from "../../src/pages/companies/_companyId/CompanyReport";
-import { firestore } from "../../src/firebase";
-import { snapshotToArray } from "../../src/utils";
 
 const UserLayout = dynamic(() => import("../../src/components/UserLayout"), {
   ssr: false,
@@ -15,24 +12,10 @@ const UserLayout = dynamic(() => import("../../src/components/UserLayout"), {
 const Reports = (props) => {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const userQuery = await firestore.collection("users").get();
-
-      let users_ = snapshotToArray(userQuery);
-      users_ = users_.map((user) => ({ ...user, userName: user.email?.split("@")?.[0] }));
-      setUsers(users_);
-    };
-
-    fetchUsers();
-  }, []);
-
   return (
     <PrivateRoutes>
       <SEOMeta {...props} />
-      <UserLayout {...props}>
-        <CompanyReport usersAdmin={users} />
-      </UserLayout>
+      <UserLayout {...props}>report</UserLayout>
     </PrivateRoutes>
   );
 };
