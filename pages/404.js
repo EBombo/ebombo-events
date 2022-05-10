e3e4d2b16f895463d489c86a0091297bd70ecbcd
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-05-10 12:00:52 -0500

    error page

--- pages/404.js
+++ pages/404.js
@@ -4,6 +4,7 @@ import { timeoutPromise } from "../src/utils/promised";
 import { useRouter } from "next/router";
 import { Image } from "../src/components/common/Image";
 import { config } from "../src/firebase";
+import { ButtonAnt } from "../src/components/form";
 
 const ErrorPage = () => {
   const router = useRouter();
@@ -21,17 +22,24 @@ const ErrorPage = () => {
   }, []);
 
   return (
-    <ErrorPageStyled>
-      <div className="content">
-        <div className="py-8">
-          <Image width="150px" src={`${config.storageUrl}/resources/white-icon-ebombo.png`} />
-        </div>
-        <h1>404</h1>
-        <div className="description">
-          <h2>This page could not be found.</h2>
+    <div className="bg-secondary bg-cover bg-no-repeat bg-pattern w-[100vw] h-[100vh] p-4 md:p-12 flex items-center">
+      <div className="grid p-4 gap-4 h-[80vh] rounded-[10px] bg-secondaryDark/[0.8] md:p-8 md:grid-cols-2">
+        <Image width="full" height="full" size="contain" margin="0" src={`${config.storageUrl}/resources/404.svg`} />
+        <div className="flex flex-col items-center md:items-start md:justify-center">
+          <div className="text-['Lato'] text-[30px] leading-[36px] font-[800] md:text-[50px] md:leading-[55px] text-white mb-4">
+            Página no encontrada
+          </div>
+          <p className="text-['Lato'] text-[20px] leading-[24px] md:text-[30px] md:leading-[36px] text-white">
+            Pero, no es culpa tuya
+          </p>
+          <ButtonAnt color="primary" onClick={() => router.push("/")}>
+            <div className="text-['Lato'] text-[16px] leading-[20px] md:text-[20px] md:leading-[24px] text-white px-8">
+              Volver al inicio
+            </div>
+          </ButtonAnt>
         </div>
       </div>
-    </ErrorPageStyled>
+    </div>
   );
 };
 
