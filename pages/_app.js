df3505f34c31f202c13ebb68b99c9428d9861792
Pablo <pablo.yncio@immertec.com>
2022-06-17 10:20:37 -0500

    add clarity

--- pages/_app.js
+++ pages/_app.js
@@ -182,6 +182,19 @@ const MyApp = ({ Component, pageProps }) => {
           `,
         }}
       />
+
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "5fkf1rchbr");
+          `,
+        }}
+      />
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <WithConfiguration>
           <WithAuthentication>
