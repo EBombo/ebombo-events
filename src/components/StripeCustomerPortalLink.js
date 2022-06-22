58b7af7ac9c584ee92bbcf718682e1e5fbd30708
anargu <anargu@pop-os.localdomain>
2022-06-22 12:35:37 -0500

    update loader logic

--- src/components/StripeCustomerPortalLink.js
+++ src/components/StripeCustomerPortalLink.js
@@ -1,46 +1,29 @@
-import React, { useState } from "reactn";
-import { useSendError } from "../hooks";
-import { Anchor, ButtonAnt } from "./form";
-import { ModalContainer } from "./common/ModalContainer";
-import { spinLoaderMin } from "./common/loader";
+import React, { useGlobal } from "reactn";
+import { useSendError, useTranslation } from "../hooks";
+import { Anchor } from "./form";
 import { goToPortalLink } from "../stripe";
-import { darkTheme } from "../theme";
 
 export const StripeCustomerPortalLink = ({ anchorWrapperClassName, anchorClassName, ...props }) => {
   const { sendError } = useSendError();
 
-  const [isLodingPortalLink, setIsLoadingPortalLink] = useState(false);
+  const { t } = useTranslation();
+
+  const [,setScreenLoader] = useGlobal("screenLoader");
 
   const goToCustomerPortal = async () => {
-    setIsLoadingPortalLink(true);
+    setScreenLoader(true);
     try {
       // Redirects to Stripe Customer Portal.
       await goToPortalLink();
     } catch (error) {
-      setIsLoadingPortalLink(false);
       sendError(error);
-      props.showNotification?.("Error", "Algo salió mal, intente dentro de unos momentos", "error");
+      props.showNotification?.(t("errors.general-title"),t("errors.general-message"), "error");
     }
+    setScreenLoader(false);
   };
 
   return (
     <>
-      <ModalContainer
-        background={darkTheme.basic.whiteLight}
-        visible={isLodingPortalLink}
-        footer={null}
-        closable
-        padding={"1rem"}
-        onCancel={() => setIsLoadingPortalLink(false)}
-      >
-        <div className="text-lg text-black">
-          <div>Se está redireccionando a la gestión de su plan</div>
-          <div>{spinLoaderMin()}</div>
-          <div className="table">
-            <ButtonAnt onClick={() => setIsLoadingPortalLink(false)}>Aceptar</ButtonAnt>
-          </div>
-        </div>
-      </ModalContainer>
       <div className={anchorWrapperClassName}>
         <Anchor {...props} onClick={() => goToCustomerPortal()}>
           <span className={anchorClassName}>{props.children}</span>
