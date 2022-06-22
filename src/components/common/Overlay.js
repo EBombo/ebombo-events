e2d36aa31125515a1d6c4c341426a0ce8aa27777
anargu <anargu@pop-os.localdomain>
2022-06-22 12:19:14 -0500

    update opacity level

--- src/components/common/Overlay.js
+++ src/components/common/Overlay.js
@@ -2,7 +2,7 @@ import React from "reactn";
 
 export const Overlay = (props) => (
   <>
-    <div className={`fixed z-40 top-0 right-0 bottom-0 left-0 bg-blackDarken bg-opacity-50 animate-fade-in`}>
+    <div className={`fixed z-40 top-0 right-0 bottom-0 left-0 bg-secondaryDark bg-opacity-70 animate-fade-in`}>
       <div className="w-full h-full flex justify-center items-center">
         {props.children}
       </div>
