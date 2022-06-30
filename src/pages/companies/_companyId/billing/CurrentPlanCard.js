967aec38362b6f43981438bd7faa79d4b4341076
anargu <anargu@pop-os.localdomain>
2022-06-30 12:00:05 -0500

    set translation

--- src/pages/companies/_companyId/billing/CurrentPlanCard.js
+++ src/pages/companies/_companyId/billing/CurrentPlanCard.js
@@ -4,8 +4,12 @@ import { ButtonAnt } from "../../../../components/form";
 import { StripeCustomerPortalLink } from "../../../../components/StripeCustomerPortalLink";
 import { spinLoaderMin } from "../../../../components/common/loader";
 import { darkTheme } from "../../../../theme";
+import { useTranslation } from "../../../../hooks";
 
 export const CurrentPlanCard = (props) => {
+
+  const { t, locale } = useTranslation("pages.billing");
+
   return (
     <PlanCardStyled className="relative">
       <div className="status-label">
@@ -16,12 +20,12 @@ export const CurrentPlanCard = (props) => {
         spinLoaderMin({ color: darkTheme.basic.white })
       ) : (
         <>
-          <div className="subheading">Plan Actual</div>
+          <div className="subheading">{t("current-plan")}</div>
           <div className="heading">{props.activePlan ? props.activePlan.name : "Free"}</div>
 
           {!props.activePlan || props.subscription?.canceled_at ? (
             <>
-              <div className="no-plan-label">¿Aún no tienes un plan?</div>
+              <div className="no-plan-label">{t("ask-not-having-plan")}</div>
               <ButtonAnt
                 block
                 color="secondary"
@@ -30,15 +34,16 @@ export const CurrentPlanCard = (props) => {
                   props.setIsSubscriptionStatusView?.(true);
                 }}
               >
-                Ver planes
+                {t("see-plans")}
               </ButtonAnt>
             </>
           ) : (
             <StripeCustomerPortalLink
               anchorWrapperClassName="absolute bottom-0 left-0 right-0 py-2 px-4 rounded-b-lg bg-secondary text-white"
               anchorClassName="text-white underline"
+              key={locale}
             >
-              Cambiar los detalles de pago
+              {t("change-detail-payment")}
             </StripeCustomerPortalLink>
           )}
         </>
