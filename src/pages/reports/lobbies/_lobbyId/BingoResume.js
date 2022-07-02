f741ac148201fb8256dcb658d3bb1a47852faffb
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-07-01 20:45:57 -0400

    bingo report resume, pattern design

--- src/pages/reports/lobbies/_lobbyId/BingoResume.js
+++ src/pages/reports/lobbies/_lobbyId/BingoResume.js
@@ -136,56 +136,82 @@ export const BingoResume = (props) => {
         </ButtonAnt>
       </div>
 
-      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[160px]">
+      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[190px]">
         <div className="flex items-center justify-between py-2 px-4 h-[40px] border-b-[1px] border-whiteDark w-full">
           <div className="text-[18px] leading-[22px] font-[700] text-grayLight">{t("patterns")}</div>
           <div className="text-[18px] leading-[22px] font-[700] text-grayLight">({props.lobby?.winners?.length})</div>
         </div>
-        <div className="w-full h-[120px] p-2 relative">
+        <div className="w-full h-[150px] p-2 md:p-4 relative">
           {props.lobby?.winners?.map((winner, index) => (
-            <div className={`w-full p-2 md:p-4 ${index === patternIndex ? "block" : "hidden"}`} key={winner.id}>
-              <table className="p-2 w-[100px] bg-secondary border-separate border-spacing-[5px] rounded-[5px]">
-                <thead>
-                  <tr>
-                    <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
-                      {props.lobby.game?.letters?.b}
-                    </th>
-                    <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
-                      {props.lobby.game?.letters?.i}
-                    </th>
-                    <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
-                      {props.lobby.game?.letters?.n}
-                    </th>
-                    <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
-                      {props.lobby.game?.letters?.g}
-                    </th>
-                    <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
-                      {props.lobby.game?.letters?.o}
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {JSON.parse(defaultTo(winner.pattern,"[]")).map((element, index) => (
-                    <tr key={index}>
-                      {element.map((value, index_) => (
-                        <td
-                          onClick={() => {
-                            if (!props.isEdit) return;
-                            editPattern(index, index_);
-                          }}
-                          key={`${pattern}-${index}-${index_}`}
-                        >
-                          <div className="square">
-                            <div className={`${value && "selected"}`} />
-                          </div>
-                        </td>
-                      ))}
+            <div className={`w-full ${index === patternIndex ? "block" : "hidden"}`} key={winner.id}>
+              <div className="flex items-center gap-4">
+                <table className="p-2 w-[100px] bg-secondary border-separate border-spacing-[5px] rounded-[5px]">
+                  <thead>
+                    <tr>
+                      <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
+                        {props.lobby.game?.letters?.b}
+                      </th>
+                      <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
+                        {props.lobby.game?.letters?.i}
+                      </th>
+                      <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
+                        {props.lobby.game?.letters?.n}
+                      </th>
+                      <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
+                        {props.lobby.game?.letters?.g}
+                      </th>
+                      <th className="text-white text-[12px] leading-[13px] font-[700] aspect-square">
+                        {props.lobby.game?.letters?.o}
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {JSON.parse(defaultTo(winner.pattern, "[]")).map((element, index) => (
+                      <tr key={index}>
+                        {element.map((value, idx) => (
+                          <td
+                            className={`w-[20%] aspect-square bg-secondary rounded-[3px]`}
+                            onClick={() => {
+                              if (!props.isEdit) return;
+                              editPattern(index, index_);
+                            }}
+                            key={`${value}-${idx}`}
+                          >
+                            <div className="aspect-square flex items-center justify-center bg-secondaryDark">
+                              <div
+                                className={`${
+                                  value && "w-[60%] aspect-square bg-whiteDark flex justify-center rounded-[50%]"
+                                }`}
+                              />
+                            </div>
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-col gap-[5px]">
+                    <div className="text-[12px] leading-[14px] font-[700] text-blackDarken">{t("called-balls")}:</div>
+                    <div className="text-[12px] leading-[14px] font-[400] text-blackDarken">{winner.lastPlays?.length}/75</div>
+                  </div>
+
+                  <div className="flex items-center gap-4">
+                    <div className="flex flex-col gap-[5px]">
+                      <div className="text-[12px] leading-[14px] font-[700] text-blackDarken">{t("duration")}:</div>
+                      <div className="text-[12px] leading-[14px] font-[400] text-blackDarken">RESPUESTA</div>
+                    </div>
+
+                    <div className="flex flex-col gap-[5px]">
+                      <div className="text-[12px] leading-[14px] font-[700] text-blackDarken">{t("time")}:</div>
+                      <div className="text-[12px] leading-[14px] font-[400] text-blackDarken">RESPUESTA</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
+
           <div
             className="cursor-pointer text-grayLight text-[12px] leading-[14px] absolute bottom-[10px] right-[10px] flex items-center"
             onClick={() =>
@@ -201,7 +227,7 @@ export const BingoResume = (props) => {
         </div>
       </div>
 
-      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[160px]">
+      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[190px]">
         <div className="flex items-center justify-between py-2 px-4 border-whiteDark border-b-[1px] w-full h-[40px]">
           <div className="text-[18px] leading-[22px] font-[700]  text-grayLight ">{`${t("empty-cards")} `}</div>
           <Tooltip placement="bottomRight" title={t("need-help-tooltip")} color="#382079">
@@ -216,7 +242,7 @@ export const BingoResume = (props) => {
         </div>
 
         {isEmpty([]) ? (
-          <div className="text-blackDarken text-[16px] leading-[19px] font-[400] h-[120px] min-w-[100%] flex items-center justify-center p-2 text-center">
+          <div className="text-blackDarken text-[16px] leading-[19px] font-[400] h-[160px] min-w-[100%] flex items-center justify-center p-2 text-center">
             {t("empty-card-message")}
           </div>
         ) : (
@@ -246,7 +272,7 @@ export const BingoResume = (props) => {
         )}
       </div>
 
-      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[160px]">
+      <div className="bg-whiteLight rounded-[4px] shadow-[2px_2px_4px_rgba(0,0,0,0.25)] h-[190px]">
         <div className="flex items-center justify-between py-2 px-4 border-whiteDark border-b-[1px] w-full h-[40px]">
           <div className="text-[18px] leading-[22px] font-[700]  text-grayLight ">{t("dropped-out")}</div>
           <Tooltip placement="bottomRight" title={t("dropped-out-tooltip")} color="#382079">
@@ -260,11 +286,11 @@ export const BingoResume = (props) => {
           </Tooltip>
         </div>
         {isEmpty(droppedOut) ? (
-          <div className="text-blackDarken text-[16px] leading-[19px] font-[400] h-[120px] min-w-[100%] flex items-center justify-center p-2 text-center">
+          <div className="text-blackDarken text-[16px] leading-[19px] font-[400] h-[150px] min-w-[100%] flex items-center justify-center p-2 text-center">
             {t("dropped-out-message")}
           </div>
         ) : (
-          <div className="h-[120px] overflow-auto p-2 flex flex-col gap-[10px]">
+          <div className="h-[150px] overflow-auto p-2 flex flex-col gap-[10px]">
             {droppedOut.map((user, index) => (
               <div className="flex items-center justify-between" key={`${user.id}-${index}`}>
                 <div className="text-blackDarken text-[14px] leading-[16px] font-[400]">
