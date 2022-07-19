6f9d12b06965c92fa4e6013698a70506ce4cbde3
Pablo <pablo.yncio@immertec.com>
2022-07-19 09:31:50 -0500

    remove unnecessary

--- src/pages/library/events/index.js
+++ src/pages/library/events/index.js
@@ -104,7 +104,7 @@ export const Events = (props) => {
                   title={
                     <div className="flex flex-col text-['Lato']">
                       <div
-                        className="flex items-center cursor-pointer text-['Lato'] p-2 text-[16px] leading-[19px] text-blackDarken"
+                        className="flex items-center cursor-pointer p-2 text-[16px] leading-[19px] text-blackDarken"
                         onClick={() => deleteEvent(event)}
                       >
                         <Image
@@ -119,7 +119,7 @@ export const Events = (props) => {
 
                       {!event.manageByUser && (
                         <div
-                          className="flex items-center cursor-pointer text-['Lato'] p-2 text-[16px] leading-[19px] text-blackDarken"
+                          className="flex items-center cursor-pointer p-2 text-[16px] leading-[19px] text-blackDarken"
                           onClick={(e) => {
                             e.preventDefault();
                             router.push(`/events/${event.id}`);
