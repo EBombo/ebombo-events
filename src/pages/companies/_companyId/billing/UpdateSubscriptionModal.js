5617ae699ed454a8f7ce66095450809080e487ff
anargu <anargu@pop-os.localdomain>
2022-07-22 16:42:50 -0500

    format

--- src/pages/companies/_companyId/billing/UpdateSubscriptionModal.js
+++ src/pages/companies/_companyId/billing/UpdateSubscriptionModal.js
@@ -18,12 +18,12 @@ export const UpdateSubscriptionModal = ({
   setIsVisibleUpdateSubscriptionModal,
   updateSubscriptionData,
   setUpdateSubscriptionData,
-  ...props}) => {
-
+  ...props
+}) => {
   const router = useRouter();
 
   const { t } = useTranslation("components.update-subscription-modal");
-  const { t : tError } = useTranslation("errors");
+  const { t: tError } = useTranslation("errors");
 
   const { sendError } = useSendError();
 
@@ -46,23 +46,24 @@ export const UpdateSubscriptionModal = ({
     fetchPreviewSubscriptionUpdate();
   }, [isVisibleUpdateSubscriptionModal]);
 
-
-  const getErrorMessage = (err) => (err?.message || err?.error || err);
+  const getErrorMessage = (err) => err?.message || err?.error || err;
 
   const fetchPreviewSubscriptionUpdate = async () => {
     try {
       // Previews the proration when updating subscription.
       // Fetch customer data to get customer StripeId.
-      const customerQuery = await firestore
-        .doc(`customers/${authUser.id}`)
-        .get();
+      const customerQuery = await firestore.doc(`customers/${authUser.id}`).get();
 
       const customer = customerQuery.data();
 
-      const { response, error } = await Fetch(`${config.serverUrl}/api/subscriptions/${subscription.id}/review`, "POST", {
-        priceId: updateSubscriptionData.price.id,
-        customerId: customer?.stripeId,
-      });
+      const { response, error } = await Fetch(
+        `${config.serverUrl}/api/subscriptions/${subscription.id}/review`,
+        "POST",
+        {
+          priceId: updateSubscriptionData.price.id,
+          customerId: customer?.stripeId,
+        }
+      );
 
       if (error) throw error;
 
@@ -102,7 +103,7 @@ export const UpdateSubscriptionModal = ({
   };
 
   const onCancelSubscriptionUpdate = () => {
-    setIsVisibleUpdateSubscriptionModal(false)
+    setIsVisibleUpdateSubscriptionModal(false);
     setDidSubscriptionUpdate(false);
 
     setUpdateSubscriptionData(null);
@@ -111,17 +112,15 @@ export const UpdateSubscriptionModal = ({
     setSubscriptionUpdateError(null);
   };
 
-  if (!subscription || !updateSubscriptionData) return (
-    <ModalContainer 
-      footer={null}
-      closable={false}
-      padding={"0rem 0rem 2rem 0rem"}
-    >
-    <div className="text-base text-danger">
-      <div>{!subscription && tError("subscription-not-found")}</div>
-      <div>{!updateSubscriptionData && tError("plan-not-found")}</div>
-    </div>
-  </ModalContainer>);
+  if (!subscription || !updateSubscriptionData)
+    return (
+      <ModalContainer footer={null} closable={false} padding={"0rem 0rem 2rem 0rem"}>
+        <div className="text-base text-danger">
+          <div>{!subscription && tError("subscription-not-found")}</div>
+          <div>{!updateSubscriptionData && tError("plan-not-found")}</div>
+        </div>
+      </ModalContainer>
+    );
 
   return (
     <ModalContainer
@@ -132,7 +131,6 @@ export const UpdateSubscriptionModal = ({
       background={darkTheme.basic.white}
       visible={isVisibleUpdateSubscriptionModal}
     >
-
       <div className="font-bold text-2xl mb-16 bg-primary text-white pt-8 px-8 pb-8">
         <div className="mb-8 min-h-[30px]">
           <Image
@@ -146,46 +144,61 @@ export const UpdateSubscriptionModal = ({
         {t("update-subscription-modal-title")}
       </div>
 
-      <div className={`text-blackDarken px-8 pb-4
-        overflow-hidden transition-height duration-300 ease-in-out ${(previewSubscriptionUpdateResponse || previewSubscriptionUpdateError) ? "max-h-[600px]" : "max-h-[200px]"}`
-      }>
+      <div
+        className={`text-blackDarken px-8 pb-4
+        overflow-hidden transition-height duration-300 ease-in-out ${
+          previewSubscriptionUpdateResponse || previewSubscriptionUpdateError ? "max-h-[600px]" : "max-h-[200px]"
+        }`}
+      >
         <div className="font-bold uppercase mb-4">{t("update-subscription-modal-changing-to")}</div>
-        {updateSubscriptionData && (<>
-          <div className="font-bold text-xl mb-8">
-            {updateSubscriptionData.plan.name}
+        {updateSubscriptionData && (
+          <>
+            <div className="font-bold text-xl mb-8">
+              {updateSubscriptionData.plan.name}
 
-            <div className="float-right text-base">{getCurrencySymbol[updateSubscriptionData.price.currency]} {amountToString(updateSubscriptionData.price.amount)}</div>
-          </div>
-        </>)}
-
-        {!previewSubscriptionUpdateResponse
-          ? (previewSubscriptionUpdateError
-            ? (<div className="text-base text-danger">
-                <div>{getErrorMessage(previewSubscriptionUpdateError)}</div>
-                <div>{tError("general-message")}</div>
-              </div>)
-            : (<div className="text-base text-grayDarken">{t("loading-preview-data-of-operation")}</div>))
-          : (<>
-            <div className="mb-4">
-              {showInvoiceLineItems && previewSubscriptionUpdateResponse.invoice.lines.data.map((lineData, index) => (
-                <div key={`line-${index}`}>
-                  {lineData.description} 
-                  <div className="float-right">
-                    {amountToString(formatAmount(lineData.amount))} {getCurrencySymbol[lineData.currency]}
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <div className="border-b-[1px] border-gray mb-8"/>
-
-            <div className=" text-base mb-12">
-              {t("amount-due")} {moment.unix(previewSubscriptionUpdateResponse.invoice.created).format(stripeDateFormat)}
-              <div className="float-right font-bold">
-                {getCurrencySymbol[previewSubscriptionUpdateResponse.invoice.currency]} {amountToString(formatAmount(previewSubscriptionUpdateResponse.invoice.amount_due))}
+              <div className="float-right text-base">
+                {getCurrencySymbol[updateSubscriptionData.price.currency]}{" "}
+                {amountToString(updateSubscriptionData.price.amount)}
               </div>
             </div>
-          </>)}
+          </>
+        )}
+
+        {!previewSubscriptionUpdateResponse ? (
+          previewSubscriptionUpdateError ? (
+            <div className="text-base text-danger">
+              <div>{getErrorMessage(previewSubscriptionUpdateError)}</div>
+              <div>{tError("general-message")}</div>
+            </div>
+          ) : (
+            <div className="text-base text-grayDarken">{t("loading-preview-data-of-operation")}</div>
+          )
+        ) : (
+          <>
+            <div className="mb-4">
+              {showInvoiceLineItems &&
+                previewSubscriptionUpdateResponse.invoice.lines.data.map((lineData, index) => (
+                  <div key={`line-${index}`}>
+                    {lineData.description}
+                    <div className="float-right">
+                      {amountToString(formatAmount(lineData.amount))} {getCurrencySymbol[lineData.currency]}
+                    </div>
+                  </div>
+                ))}
+            </div>
+
+            <div className="border-b-[1px] border-gray mb-8" />
+
+            <div className=" text-base mb-12">
+              {t("amount-due")}{" "}
+              {moment.unix(previewSubscriptionUpdateResponse.invoice.created).format(stripeDateFormat)}
+              <div className="float-right font-bold">
+                {getCurrencySymbol[previewSubscriptionUpdateResponse.invoice.currency]}{" "}
+                {amountToString(formatAmount(previewSubscriptionUpdateResponse.invoice.amount_due))}
+              </div>
+            </div>
+          </>
+        )}
 
         {subscriptionUpdateError && (
           <div className="text-base text-danger">
@@ -218,21 +231,21 @@ export const UpdateSubscriptionModal = ({
           </ButtonAnt>
 
           <ButtonAnt
-                margin="20px auto auto auto"
-                variant="contained"
-                color="primary"
-                size="big"
-                fontSize="14px"
-                fontWeight="bold"
-                width="150px"
-                disabled={!previewSubscriptionUpdateResponse || didSubscriptionUpdate}
-                loading={isLoadingUpdateSubscription}
-                onClick={(e) => {
-                  e.preventDefault();
-                  updateSubscription();
-                }}
-              >
-                {t("confirm")}
+            margin="20px auto auto auto"
+            variant="contained"
+            color="primary"
+            size="big"
+            fontSize="14px"
+            fontWeight="bold"
+            width="150px"
+            disabled={!previewSubscriptionUpdateResponse || didSubscriptionUpdate}
+            loading={isLoadingUpdateSubscription}
+            onClick={(e) => {
+              e.preventDefault();
+              updateSubscription();
+            }}
+          >
+            {t("confirm")}
           </ButtonAnt>
         </div>
       </div>
